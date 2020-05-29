fc26db68702f5828e2f65b594d5b9f6e6d8fd332
SHANTHI SAMBOJU <ssamboju@in.ibm.com>
2020-05-29 18:59:20 +0530

    commit my python code

---
+++ healthplannerPythonAPI-master/routes/.ipynb_checkpoints/request_api-checkpoint.py
@@ -0,0 +1,98 @@
+"""The Endpoints to manage the Health record requests"""
+
+import copy
+from flask import json, jsonify, abort, request, Blueprint
+from validate_email import validate_email
+from bson import json_util
+
+# import the MongoClient classes
+from pymongo import MongoClient, errors
+
+REQUEST_API = Blueprint('request_api', __name__)
+
+# read property file for attributes
+propertydict = {}
+with open(r"C:/Users/SHANTHISAMBOJU/Python_training_13th_17th_April_2020/HACATHON_Project_20th_May/healthplannerPythonAPI-master/static/properties.txt") as pfile:
+    for line in pfile:
+        key, value = line.partition("=")[::2]
+        propertydict[key] = value.strip()
+properties = type("Names", (object,), propertydict)
+print("MongoDB URL:", properties.mongodburl)
+print("MongoDB database name:", properties.mongodbname)
+
+# instantiate MongoDb connection and database
+client = MongoClient(properties.mongodburl)
+#client = MongoClient('mongodb://root:root@localhost:27017')
+print("client name",client)
+db = client.get_database(properties.mongodbname)
+print("db name:",db)
+
+Collection=db.getCollectionNames();# added to get the collections in current db
+users=db.getUsers()
+
+patientrecords = db.collection
+print("****************************\npatient list in the mongodb dabase{} is {}\n************************ ".format(properties.mongodbname,patientrecords))
+
+# print the version of MongoDB server if connection successful
+print("MongoDB server version:", client.server_info()["version"])
+
+def get_blueprint():
+    """Return the blueprint for the main app module"""
+    return REQUEST_API
+
+@REQUEST_API.route('/request', methods=['GET'])
+def get_records():
+    # Retrieve all patient records
+    patients = patientrecords.find()
+    PATIENT_LIST = []
+    for patient in patients:
+        del patient['_id'] # Object id is not JSON serializable
+        PATIENT_LIST.append(patient)
+    return jsonify(PATIENT_LIST)
+
+@REQUEST_API.route('/request/<string:_id>', methods=['GET'])
+def get_record_by_id(_id):
+    patient = patientrecords.find_one({"id":_id})
+    if not patient:
+        abort(404)
+    # Return with a 404 error if id does not exist
+    del patient['_id'] # Object id is not JSON serializable
+    foundPatient = copy.deepcopy(patient)
+    return jsonify(foundPatient)
+
+@REQUEST_API.route('/request', methods=['POST'])
+def create_record():
+    if not request.get_json():
+        abort(400, description="Request has incorrect JSON format")
+    data = request.get_json(force=True)
+    if not data.get('id'):
+        abort(400, description="Request does not have id")
+    if not validate_email(data['mailId']):
+        abort(400, description="Request email has incorrect email format")
+    newPatient = copy.deepcopy(data)
+    patientrecords.insert_one(newPatient)
+    # HTTP 201 Created
+    return jsonify({"id": data.get('id')}), 201
+
+@REQUEST_API.route('/request/<string:_id>', methods=['PUT'])
+def edit_record(_id):
+    if not request.get_json():
+        abort(400)
+    data = request.get_json(force=True)
+    if not data['id']:
+        abort(400)
+    if not validate_email(data['mailId']):
+        abort(400)
+    if not patientrecords.find_one({"id":_id}):
+        abort(404)
+    updatedPatient = copy.deepcopy(data)
+    patientrecords.replace_one({'id':_id}, updatedPatient)
+    # HTTP 201 Created
+    return jsonify(updatedPatient), 201
+
+@REQUEST_API.route('/request/<string:_id>', methods=['DELETE'])
+def delete_record(_id):
+    if not patientrecords.find_one({"id":_id}):
+        abort(404)
+    patientrecords.delete_one({'id':_id})
+    return '', 204
